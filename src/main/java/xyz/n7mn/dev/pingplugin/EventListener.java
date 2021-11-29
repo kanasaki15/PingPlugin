329f1403979bd1be2830c2f70222fe7242610063
tina-chan <kassi_0123@kassi-hp.info>
2021-11-30 01:40:45 +0900

    1.18対応

--- src/main/java/xyz/n7mn/dev/pingplugin/EventListener.java
+++ src/main/java/xyz/n7mn/dev/pingplugin/EventListener.java
@@ -12,6 +12,12 @@ public class EventListener implements Listener {
     public void ProxyPingEvent (ProxyPingEvent e){
         ServerPing.Protocol version = e.getResponse().getVersion();
         version.setName("7mi-sys 1.8-1.18");
+
+        // minecraft.jp対策
+        int mine1_18 = 756; //TODO: 正式リリース後確認
+        if (!(version.getProtocol() >= 47 && version.getProtocol() <= mine1_18)){
+            version.setProtocol(mine1_18);
+        }
         e.getResponse().setVersion(version);
     }
 
